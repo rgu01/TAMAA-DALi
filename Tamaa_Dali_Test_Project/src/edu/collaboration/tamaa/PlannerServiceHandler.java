e479ab4c669e2becce2ed18d5f6857d3c1c2fe1c
rong <rong@rong>
2020-04-09 14:55:24 +0200

    Gu

--- Tamaa_Dali_Test_Project/src/edu/collaboration/tamaa/PlannerServiceHandler.java
+++ Tamaa_Dali_Test_Project/src/edu/collaboration/tamaa/PlannerServiceHandler.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-//added from linux
+//change 
 
 public class PlannerServiceHandler implements PlannerService.Iface {
 
